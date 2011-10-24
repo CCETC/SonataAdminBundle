f7dcf7b3cb6ac1cfe4bb7f757db93030768c18f8
Pat Haggerty <haggertypat@gmail.com>
2011-10-24 11:39:44 -0400

    tinymce

--- Resources/public/js/events.js
+++ Resources/public/js/events.js
@@ -19,4 +19,24 @@ $(document).ready(function() {
         }
         
     });
+    
+});
+
+
+
+tinyMCE.init({
+        // General options
+        mode : "textareas",
+        theme : "advanced",
+        plugins : ",advimage,iespell,inlinepopups,insertdatetime,paste",
+
+        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontsizeselect",
+        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,image,|,forecolor,backcolor",
+        theme_advanced_buttons3 : "",
+        theme_advanced_buttons4 : "",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_resizing : true
+
+  
 });
