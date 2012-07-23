77fcce7b418322378a8a3a35e507a5b558346824
Pat Haggerty <haggertypat@gmail.com>
2012-07-23 12:55:03 -0400

    collapsed filters, made interactive

--- Resources/public/js/events.js
+++ Resources/public/js/events.js
@@ -5,21 +5,35 @@ $(document).ready(function() {
            console.log($(this).parent().siblings('ul.inputs-list'));
            $(this).parent().siblings('ul.inputs-list').find("label input[type='checkbox']").attr('checked', $(this).is(':checked'));
        });
+
+       $('.filter-results-toggle').live('click', function(e) {
+           e.preventDefault();
+           $(this).hide();
+           $('tr.filter-container.inactive').show();
+           $('table.filter-table').show();
+           $('#submit-filters').show();
+           $('.filter-results-cancel').show();           
+       });
+       $('.filter-results-cancel').live('click', function(e) {
+           e.preventDefault();
+           $(this).hide();
+           $('table.filter-table').hide();
+           $('#submit-filters').hide();
+           $('.filter-results-toggle').show();
+       });
+       $('.more-filters-toggle').live('click', function(e) {
+           e.preventDefault();
+           $(this).hide();
+           console.log($('tr.filter-container.inactive'));
+           $('tr.filter-container.inactive').show();
+           $('.more-filters-cancel').show();
+       });
+       $('.more-filters-cancel').live('click', function(e) {
+           e.preventDefault();
+           $(this).hide();
+           $('tr.filter-container.inactive').hide();
+           $('.more-filters-toggle').show();
+       });
+
 }); 
 
-tinyMCE.init({
-        // General options
-        mode : "specific_textareas",
-        editor_selector : "tinymce",
-        theme : "advanced",
-        plugins : "advimage,iespell,inlinepopups,insertdatetime,paste",
-
-        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontsizeselect",
-        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,bullist,numlist,hr,|,outdent,indent,|,undo,redo,|,link,unlink,image,|,forecolor,backcolor",
-        theme_advanced_buttons3 : "",
-        theme_advanced_buttons4 : "",
-        theme_advanced_toolbar_location : "top",
-        theme_advanced_toolbar_align : "left",
-        theme_advanced_statusbar_location : "bottom",
-        theme_advanced_resizing : true
-});
